205b46d2e13390983cee4b1881a9f03716f1be0d
chohyejung <hj1021007@gmail.com>
2025-08-20 03:30:45 +0900

    feat: AuthAccount Entity 생성

---
+++ src/main/java/com/goormi/routine/domain/auth/entity/AuthAccount.java
@@ -0,0 +1,43 @@
+package com.goormi.routine.domain.auth.entity;
+
+import com.goormi.routine.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "auth_accounts")
+@Getter @Setter
+@NoArgsConstructor
+public class AuthAccount {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "auth_id")
+	private Long authId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Column(name = "kakao_id", nullable = false, unique = true)
+	private String kakaoId;
+
+	@Column(name = "refresh_token")
+	private String refreshToken;
+
+	@Column(name = "is_active", nullable = false)
+	private Boolean isActive = true;
+
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+}
