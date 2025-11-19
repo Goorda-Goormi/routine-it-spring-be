0de5743d2b058c7c4428fe2f29bd2eb60d207784
chaanheeLEE <chanhee1519@gmail.com>
2025-11-19 18:23:56 +0900

    refact: Pageable 적용

--- src/main/java/com/goormi/routine/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/goormi/routine/domain/notification/repository/NotificationRepository.java
@@ -2,6 +2,8 @@ package com.goormi.routine.domain.notification.repository;
 
 import com.goormi.routine.domain.notification.entity.Notification;
 import com.goormi.routine.domain.notification.entity.NotificationType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,10 +17,10 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
             "JOIN FETCH n.sender " +
             "LEFT JOIN FETCH n.group " +
             "WHERE n.receiver.id = :receiverId ORDER BY n.createdAt DESC")
-    List<Notification> findByReceiver_IdOrderByCreatedAtDesc(@Param("receiverId") Long receiverId);
+    Page<Notification> findByReceiver_IdOrderByCreatedAtDesc(@Param("receiverId") Long receiverId, Pageable pageable);
     @Query("SELECT n FROM Notification n " +
             "JOIN FETCH n.sender " +
             "LEFT JOIN FETCH n.group " +
             "WHERE n.receiver.id = :receiverId AND n.notificationType = :type ORDER BY n.createdAt DESC")
-    List<Notification> findByReceiver_IdAndNotificationType(@Param("receiverId") Long receiverId, @Param("type") NotificationType type);
+    Page<Notification> findByReceiver_IdAndNotificationType(@Param("receiverId") Long receiverId, @Param("type") NotificationType type, Pageable pageable);
 }
